f2ac0f46ccb100e1a3f2eccc90a5e4e9ed066ae3
dimon <dimagol64@gmail.com>
2020-04-20 20:20:17 +0300

    Updates

---
+++ precache-manifest.829f8f2107de432ec1d01400391ed140.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b3d87eaf33176bc1e25b5ed9a208718",
+    "url": "/react-toDoList/index.html"
+  },
+  {
+    "revision": "71fad8313e518513697d",
+    "url": "/react-toDoList/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "d83bc6cff1e26129614c",
+    "url": "/react-toDoList/static/css/main.6750a390.chunk.css"
+  },
+  {
+    "revision": "71fad8313e518513697d",
+    "url": "/react-toDoList/static/js/2.3b33f393.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-toDoList/static/js/2.3b33f393.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d83bc6cff1e26129614c",
+    "url": "/react-toDoList/static/js/main.c8587f3c.chunk.js"
+  },
+  {
+    "revision": "2704d26a14657638b5a1",
+    "url": "/react-toDoList/static/js/runtime-main.f40f7026.js"
+  }
+]);
